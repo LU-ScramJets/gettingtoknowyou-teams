1d05fa10397133a5eb33ca3bb4041c1aa8934144
Jefferson C <jcherton@hotmail.com>
2023-10-04 05:51:03 -0500

    managed to miss a curly brace in the script.

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 function orig(){
         document.mapBG.src="assets/scramjets_grouppic_bw.png"
     }
-    function who(x){
-        switch(x)
-        {
-            case "zee":
-                document.mapBG.src="assets/scramjets_grouppic_zee.png"
-            break;
+function who(x){
+    switch(x)
+    {
+        case "zee":
+            document.mapBG.src="assets/scramjets_grouppic_zee.png"
+        break;
 
-            case "alex":
-                document.mapBG.src="assets/scramjets_grouppic_alex.png"
-            break;
+        case "alex":
+            document.mapBG.src="assets/scramjets_grouppic_alex.png"
+        break;
 
-            case "joey":
-                document.mapBG.src="assets/scramjets_grouppic_joey.png"
-            break;
+        case "joey":
+            document.mapBG.src="assets/scramjets_grouppic_joey.png"
+        break;
 
-            case "yjefe":
-                document.mapBG.src="assets/scramjets_grouppic_yjefe.png"
-            break;
+        case "yjefe":
+            document.mapBG.src="assets/scramjets_grouppic_yjefe.png"
+        break;
 
-            case "james":
-                document.mapBG.src="assets/scramjets_grouppic_james.png"
-            break;
+        case "james":
+            document.mapBG.src="assets/scramjets_grouppic_james.png"
+        break;
 
-        }
+    }
+}
+	
